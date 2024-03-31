973a0981f79d90ce1174916d7962abe08acb0145
CORP\cmingora <cristian.mingorance-mulero@capgemini.com>
2024-03-31 21:39:31 +0200

    🚀 Start Project

---
+++ src/main/java/org/zara/ecommercepriceserviceapi/ECommercePriceServiceApiApplication.java
@@ -0,0 +1,13 @@
+package org.zara.ecommercepriceserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePriceServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommercePriceServiceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zara/ecommercepriceserviceapi/ECommercePriceServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zara.ecommercepriceserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommercePriceServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
